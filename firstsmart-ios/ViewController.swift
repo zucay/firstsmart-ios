7ef252e2dd6cff72598b5af8206f9c2c15f28be1
yukihico <yukihico@gmail.com>
2015-08-19 19:26:09 +0900

    画面サイズを端末で調整

--- firstsmart-ios/ViewController.swift
+++ firstsmart-ios/ViewController.swift
@@ -12,6 +12,7 @@ import SIOSocket
 class ViewController: UIViewController {
     var socket:SIOSocket! = nil
     @IBOutlet weak var webView: UIWebView!
+    let safariUASuffix = " Safari/600.1.4 "
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +35,17 @@ class ViewController: UIViewController {
             self.socket.on("msg", callback: {(data:[AnyObject]!)  in
                 let url = data[0] as? String
                 println(url!)
+                
+                self.webView.frame = UIScreen.mainScreen().bounds
+                self.webView.center = self.view.center
+                
+                
+                let userAgent:String! = self.webView.stringByEvaluatingJavaScriptFromString("navigator.userAgent")
+                let customUserAgent:String = userAgent.stringByAppendingString(self.safariUASuffix)
+                let dic:NSDictionary = ["UserAgent":customUserAgent]
+                NSUserDefaults.standardUserDefaults().registerDefaults(dic as [NSObject:AnyObject])
+                
+                
                 self.webView.loadRequest(NSURLRequest(URL: NSURL(string: url!)!))
             })
             
