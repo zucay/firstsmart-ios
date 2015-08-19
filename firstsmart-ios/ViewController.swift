82a8d0bbc93048f4111abc2fe0069eb7e876d271
yukihico <yukihico@gmail.com>
2015-08-19 20:06:04 +0900

    refactoring & reconnect対応

--- firstsmart-ios/ViewController.swift
+++ firstsmart-ios/ViewController.swift
@@ -17,40 +17,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        SIOSocket.socketWithHost("wss://firstsmart-nodejs.herokuapp.com/", response:  { (_socket: SIOSocket!) in
-            self.socket = _socket
-            
-            
-            self.socket.onConnect = {() in
-                println("connected")
-                
-                self.socket.emit("joinRoom", args: ["myroom"])
-                // self.socket.emit("msg", args: ["hoge"])
-            }
-            
-            self.socket.onDisconnect = {() in
-                println("disconnected")
-            }
-            
-            self.socket.on("msg", callback: {(data:[AnyObject]!)  in
-                let url = data[0] as? String
-                println(url!)
-                
-                self.webView.frame = UIScreen.mainScreen().bounds
-                self.webView.center = self.view.center
-                
-                self.webView.loadRequest(NSURLRequest(URL: NSURL(string: url!)!))
-            })
-            
-            
-        })
-        
+        webViewInit()
+        socketInit()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    private func webViewInit() {
+        self.webView.frame = UIScreen.mainScreen().bounds
+        self.webView.center = self.view.center
+    }
+    
+    private func socketInit() {
+        SIOSocket.socketWithHost("wss://firstsmart-nodejs.herokuapp.com/", reconnectAutomatically: true, attemptLimit: 10, withDelay: 3, maximumDelay: 120, timeout: 10, response:  { (_socket: SIOSocket!) in
+            self.socket = _socket
+            
+            self.socket.onConnect = {() in
+                println("connected")
+                
+                self.socket.emit("joinRoom", args: ["myroom"])
+            }
+            
+            self.socket.onReconnect = {(attempts: Int) in
+                println("re-connected")
+            }
+            
+            self.socket.onDisconnect = {() in
+                println("disconnected")
+                
+            }
+            
+            self.socket.on("msg", callback: {(data:[AnyObject]!)  in
+                let url = data[0] as? String
+                println(url!)
+                
+                self.webView.loadRequest(NSURLRequest(URL: NSURL(string: url!)!))
+            })
+        })
+    }
+    
 
 
 }
